033ceb1b7bf9ae627a40ce339153873518455312
Emily Goetz <emily@emilygoetz.com>
2015-11-21 16:51:50 -0500

    webview

--- app/index.ios.js
+++ app/index.ios.js
@@ -10,23 +10,13 @@ var {
   StyleSheet,
   Text,
   View,
+  WebView
 } = React;
 
 var Dinotes = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <WebView url="http://google.com" />
     );
   }
 });
