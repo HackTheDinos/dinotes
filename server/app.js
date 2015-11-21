b9e46214e7989b6d16d36b49aa9063f23961f3c2
William Jagels <will.jagels@gmail.com>
2015-11-21 17:40:48 -0500

    Statically serve out of client folder

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +21,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(session({
+    genid: function(req) {
+        return genuuid() // use UUIDs for session IDs
+    },
+    secret: 'hack the dinos ajdsglkjaoijoinekjcajsiudhfiewajflkanckjhaiuewjinewf'
+}));
 
 app.use('/', routes);
 app.use('/users', users);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
 });
 
 module.exports = router;
