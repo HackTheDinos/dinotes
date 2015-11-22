bf43adce5700a79841d4e2172a1f467dbd33f2d9
Yining Chen <yining.chen@nyu.edu>
2015-11-22 07:11:45 -0500

    hooked up backend to front end. Also mobile input types

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,67 @@
-$( document ).ready(function(){
-    $(".button-collapse").sideNav();
-})
+$(document).ready(function() {
+   var userCardResults;
+
+   // Fetch cards from backend
+   userCardResults = getRecordsFrom('/notes/mine');
+
+   // Remove null records based on a key to check
+   userCardResults = returnValidCards(userCardResults.owned,
+      'speciman-card-speciman-name');
+
+   // Render validated records w/ Mustache into HTML
+   renderMustache(
+      'js/template-speciman-card.mst',
+      userCardResults,
+      '#loaded-speciman-cards');
+
+   $(".button-collapse").sideNav();
+});
+
+/**
+ * fetch from backend using express route
+ * @param  {[String]} expressRoute
+ * @return {[JSON]}
+ */
+function getRecordsFrom(expressRoute) {
+   return JSON.parse($.ajax({
+      type: "GET",
+      url: expressRoute,
+      async: false
+   }).responseText);
+}
+
+/**
+ * @param  {[String]} templatePath
+ * @param  {[Array]} data        data to render
+ * @param  {[String]} divId        div id to render into
+ */
+function renderMustache(templatePath, data, divId) {
+   var rendered = '';
+   var i;
+
+   $.get(templatePath, function(template) {
+      for (i = 0; i < data.length; i++) {
+         rendered += Mustache.render(template, data[i].contents);
+      }
+      $(divId).html(rendered);
+   });
+}
+
+/**
+ * remove null records
+ * @param  {[Array]} allCards   
+ * @param  {[String]} keyToCheck check this key for null value
+ * @return {[Array]}            of valid records
+ */
+function returnValidCards(allCards, keyToCheck) {
+   var validCards = [];
+   var i;
+
+   for (i = 0; i < allCards.length; i++) {
+      if (allCards[i].contents[keyToCheck]) {
+         validCards.push(allCards[i]);
+      }
+   }
+
+   return validCards;
+}
